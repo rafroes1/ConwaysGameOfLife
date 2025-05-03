7ead729c14fcfc2995d79afcd706d2eba1bdf5de
rafroes1 <rafafroes@outlook.com>
2025-05-03 10:47:38 -0300

    Initial Commit

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Rafael Carvalho on 03/05/25.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
